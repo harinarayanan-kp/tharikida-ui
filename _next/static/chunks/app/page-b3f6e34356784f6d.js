dd57e35d9f687e3bf4bece60f74928dc5768338d
Harinarayanan K P <harikpaug18@gmail.com>
2024-12-17 12:02:33 +0530

    Updates

--- _next/static/chunks/app/page-b3f6e34356784f6d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{6324:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,6084,23))},6084:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[565,173,441,517,358],()=>s(6324)),_N_E=e.O()}]);
