6795ce58ca6609a685fb61f986b5042255234bba
Harinarayanan K P <harikpaug18@gmail.com>
2025-07-05 11:17:46 +0530

    fix: Update package description for clarity and remove unused components from index export

--- tharikida-ui/src/index.ts
+++ tharikida-ui/src/index.ts
@@ -1,5 +1,3 @@
 import ThemeProvider from "./theme/ThemeProvider";
-import AnimatedBox from "./components/Specials/AnimatedBox";
-import MusicCard from "./components/Specials/MusicCard";
 
-export { ThemeProvider, AnimatedBox, MusicCard };
+export { ThemeProvider};
